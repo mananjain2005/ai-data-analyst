470a6f6682836c4d206cea9da19737346bfbde6a
Manan Jain <mananjain2018@gmail.com>
2026-06-16 14:22:08 +0530

    Updates

--- src/llm.py
+++ src/llm.py
@@ -10,8 +10,8 @@ def get_llm():
     """
 
     llm = ChatGroq(
-        # model="llama-3.3-70b-versatile",
-        model="llama-3.1-8b-instant",
+        model="llama-3.3-70b-versatile",
+        # model="llama-3.1-8b-instant",
         temperature=0
     )
 
